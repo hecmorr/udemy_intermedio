f302a49ef0260f25a42ebac98d85aaa9df29ebf8
Hector Morales <hectormorales@Hectors-MacBook-Pro.local>
2023-08-28 14:13:28 -0600

    Initial commit from miscelaneos

---
+++ miscelaneos/lib/app/view/home_page.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class HomeView extends StatelessWidget {
+  const HomeView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const HomePage();
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text('Flutter app'),
+      ),
+    );
+  }
+}
+
---
+++ miscelaneos/lib/main_production.dart
@@ -0,0 +1,6 @@
+import 'package:miscelaneos/app/app.dart';
+import 'package:miscelaneos/bootstrap.dart';
+
+void main() {
+  bootstrap(() => const App());
+}
